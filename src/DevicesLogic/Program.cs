8f962552263cdf438cd91f4714bf39e80fd0c80c
aniaania321 <s31154@pjwstk.edu.pl>
2025-04-23 00:57:43 +0200

    Net 8.0 finally works on new PC and database is connected

--- src/DevicesLogic/Program.cs
+++ src/DevicesLogic/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
 
-Console.WriteLine("Hello, World!");
+string connectionString = "Data Source=db-mssql;Initial Catalog=2019SBD;Integrated Security=True;Trust Server Certificate=True";
+
+using var connection = new SqlConnection(connectionString);
+connection.Open();
+
+var command = new SqlCommand("SELECT * FROM EmbeddedDevice", connection);
+var reader = command.ExecuteReader();
+
+while (reader.Read())
+{
+    Console.WriteLine($"{reader["Id"]}, {reader["Name"]}, {reader["IpAddress"]}");
+}
